308d8ec3929a83e848307e226006ced0c61ff6a9
MoazMirza-13 <moazmirza13@gmail.com>
2023-07-28 18:55:36 +0500

    improved footer

--- components/Footer.js
+++ components/Footer.js
@@ -152,7 +152,7 @@ export default function Footer() {
                 duration: 0.4,
                 type: "spring",
                 stiffness: 120,
-                delay: 1.9,
+                delay: 1.95,
               }}
               className="font-mont font-medium text-center text-xs text-[#646464]"
             >
